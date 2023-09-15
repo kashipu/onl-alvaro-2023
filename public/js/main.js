e53164cf01315b67ae85fc59fc624dc360d9fff9
kashipu <graficowm@gmail.com>
2023-09-15 16:47:41 -0500

    Solucionado background capsulas, slider capsulas, botón enlaces

--- public/js/main.js
+++ public/js/main.js
@@ -30,3 +30,39 @@ $('.slider-cards-indicadores').slick({
       }
     ]
   });
+
+  $('.capsulas-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 792,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
